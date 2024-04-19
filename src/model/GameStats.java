bd0fd50ab6cc50000ad55cb89590f64d357ff2f1
Milt Rue <miltonrue03@gmail.com>
2024-04-19 15:09:07 -0700

    GameStats

---
+++ src/model/GameStats.java
@@ -0,0 +1,32 @@
+package model;
+
+public class GameStats {
+	private int numClicks;
+	private double time;
+	private Game.Difficulty dif;
+	private int numCards;
+	
+	public GameStats(int numGuesses, double numSeconds, Game.Difficulty difficulty, int numberOfCards) {
+		this.numClicks = numGuesses;
+		this.time = numSeconds;
+		this.dif = difficulty;
+		this.numCards = numberOfCards;
+	}
+	
+	public int getNumClicks() {
+		return numClicks;
+	}
+	
+	public double getTime() {
+		return time;
+	}
+	
+	public Game.Difficulty getDifficulty() {
+		return dif;
+	}
+	
+	public int getNumCards() {
+		return numCards;
+	}
+
+}
