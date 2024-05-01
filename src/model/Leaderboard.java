fa85669a96da605b97830dc7c4b1e70b2adea222
shashanksraj <shashankraj1412@gmail.com>
2024-05-01 05:36:51 -0700

    Java docs

--- src/model/Leaderboard.java
+++ src/model/Leaderboard.java
@@ -3,35 +3,74 @@ package model;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Class representing a leaderboard entry with player name and score properties.
+ */
 public class Leaderboard {
-    private final SimpleStringProperty playerName;
-    private final SimpleIntegerProperty score;
+    private final SimpleStringProperty playerName;  // Property for player's name to support data binding
+    private final SimpleIntegerProperty score;      // Property for player's score to support data binding
 
+    /**
+     * Constructs a new leaderboard entry with specified player name and score.
+     * 
+     * @param playerName the name of the player
+     * @param score the score of the player
+     */
     public Leaderboard(String playerName, int score) {
         this.playerName = new SimpleStringProperty(playerName);
         this.score = new SimpleIntegerProperty(score);
     }
 
+    /**
+     * Gets the player's name.
+     * 
+     * @return the name of the player
+     */
     public String getPlayerName() {
         return playerName.get();
     }
 
+    /**
+     * Sets the player's name.
+     * 
+     * @param playerName the new name to set
+     */
     public void setPlayerName(String playerName) {
         this.playerName.set(playerName);
     }
 
+    /**
+     * Gets the player's score.
+     * 
+     * @return the score of the player
+     */
     public Integer getScore() {
         return score.get();
     }
 
+    /**
+     * Sets the player's score.
+     * 
+     * @param score the new score to set
+     */
     public void setScore(Integer score) {
         this.score.set(score);
     }
 
+    /**
+     * Provides a property wrapper for the player's name. This is used for data binding in JavaFX.
+     * 
+     * @return the property for the player's name
+     */
     public SimpleStringProperty playerNameProperty() {
         return playerName;
     }
 
+    /**
+     * Provides a property wrapper for the player's score. This is used for data binding in JavaFX.
+     * 
+     * @return the property for the player's score
+     */
     public SimpleIntegerProperty scoreProperty() {
         return score;
     }
--- src/model/account/AccountManager.java
+++ src/model/account/AccountManager.java
@@ -1,38 +1,62 @@
 package model.account;
 
 import java.util.HashMap;
-import java.util.List;
 
 public class AccountManager {
 
-	private Account loggedInAccount;
-	private HashMap<String, Account> accounts = new HashMap();
+    private Account loggedInAccount;  // Holds the currently logged-in account
+    private HashMap<String, Account> accounts = new HashMap<>();  // Maps usernames to their respective Account objects
 
-	public boolean login(String name, String password) {
-		Account account = accounts.get(name);
-			if(account != null && account.correctPassword(password)) {
-				loggedInAccount = account;
-				System.out.print("Login successful");
-				return true;
-			}
-			System.out.println("Login failed");
-			return false;
-		}
-	
-	
-	public boolean userIsLoggedIn() {
-		return loggedInAccount != null;
-	}
-	
-	public void loggedOut() {
-		loggedInAccount = null;
-	}
-	
-	public Account getLoggedInAccount() {
-		return loggedInAccount;
-	}
+    /**
+     * Attempts to log in a user with the provided username and password.
+     * 
+     * @param name the username of the account
+     * @param password the password of the account
+     * @return true if login is successful, false otherwise
+     */
+    public boolean login(String name, String password) {
+        Account account = accounts.get(name);
+        if (account != null && account.correctPassword(password)) {
+            loggedInAccount = account;
+            System.out.print("Login successful");
+            return true;
+        }
+        System.out.println("Login failed");
+        return false;
+    }
 
+    /**
+     * Checks if any user is currently logged in.
+     * 
+     * @return true if a user is logged in, false otherwise
+     */
+    public boolean userIsLoggedIn() {
+        return loggedInAccount != null;
+    }
 
+    /**
+     * Logs out the current user by setting the loggedInAccount to null.
+     */
+    public void loggedOut() {
+        loggedInAccount = null;
+    }
+
+    /**
+     * Retrieves the currently logged-in account.
+     * 
+     * @return the logged-in Account object, or null if no user is logged in
+     */
+    public Account getLoggedInAccount() {
+        return loggedInAccount;
+    }
+
+    /**
+     * Creates a new account with the specified username and password.
+     * 
+     * @param userName the username for the new account
+     * @param password the password for the new account
+     * @return true if the account was created successfully, false if username is taken or inputs are invalid
+     */
     public boolean createAccount(String userName, String password) {
         if (userName == null || userName.isEmpty()) {
             System.out.println("Username cannot be empty");
@@ -42,7 +66,7 @@ public class AccountManager {
             System.out.println("Password cannot be empty");
             return false;
         }
-        if(accounts.containsKey(userName)) {
+        if (accounts.containsKey(userName)) {
             System.out.println("Username is already taken");
             return false;
         }
@@ -51,9 +75,12 @@ public class AccountManager {
         return true;
     }
 
-
-	public HashMap<String, Account> getAccounts() {
-		// TODO Auto-generated method stub
-		return accounts;
-	}
+    /**
+     * Gets a map of all accounts managed by this AccountManager.
+     * 
+     * @return a map of username strings to Account objects
+     */
+    public HashMap<String, Account> getAccounts() {
+        return accounts;
+    }
 }
